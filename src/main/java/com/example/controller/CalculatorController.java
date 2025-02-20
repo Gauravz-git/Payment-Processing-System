5ec66c625c3de6b5fd5c99a5f14c4d26a5375db3
gauravchaudhari-git <chaudhari.gaurav.comp@kgce.edu.in>
2025-02-20 20:51:39 +0530

    Demo added

---
+++ src/main/java/com/example/controller/CalculatorController.java
@@ -0,0 +1,23 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+
+    @GetMapping("/add")
+    public int add(@RequestParam int a, @RequestParam int b) {
+        return a + b;
+    }
+
+    @GetMapping("/sub")
+    public int subtract(@RequestParam int a, @RequestParam int b) {
+        return a - b;
+    }
+
+    @GetMapping("/mul")
+    public int multiply(@RequestParam int a, @RequestParam int b) {
+        return a * b;
+    }
+}
