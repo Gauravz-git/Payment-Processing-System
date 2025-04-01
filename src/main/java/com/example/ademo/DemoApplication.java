009a963d1d09d721c73b3fb456f5e200169f1795
Gauravz-git <gauravc0223@gmail.com>
2025-04-02 00:27:25 +0530

    changed source code of CalculatorController.java file

--- src/main/java/com/example/ademo/DemoApplication.java
+++ src/main/java/com/example/ademo/DemoApplication.java
@@ -13,8 +13,10 @@ public class DemoApplication {
     public static void main(String[] args) {
       SpringApplication.run(DemoApplication.class, args);
     }
+    
+    
     @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public String hello(@RequestParam(defaultValue = "World") String name) {
       return String.format("Hello %s!", name);
     }
 }
--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.example.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import com.example.ademo.pojo.MyNum;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -18,18 +17,18 @@ public class TestController {
             @PathVariable String pv1,
             @PathVariable String pv2,
             @PathVariable String pv3,
-            @RequestBody myNum requestBody) {
+            @RequestBody String requestBody) {
 
-        System.out.println("Running myMethodMPost(): " + param1 + " + " + param2 + " + " + pv1 + " + " + pv2 + " + " + pv3 + " + " + requestBody);
+        System.out.println("Running myMethodpost(): " + param1 + " + " + param2 + " + " + pv1 + " + " + pv2 + " + " + pv3 + " + " + requestBody);
 
-        /*  // Convert JSON using Jackson
+        // Convert JSON using Jackson
         MyNum reqAsObjViaJackson = convertJsonToMyNum(requestBody);
         System.out.println("reqAsObj: " + reqAsObjViaJackson);
 
         // Convert JSON using Gson
         MyNum reqAsObjViaGson = convertJsonToMyNumUsingGson(requestBody);
         System.out.println("reqAsObjViaGson: " + reqAsObjViaGson);
-        */       
+
         return "Hello World-post|param1:" + param1 + "|param2:" + param2
                 + "|pv1:" + pv1 + "|pv2:" + pv2 + "|pv3:" + pv3
                 + "\n|requestBody:\n" + requestBody;
@@ -37,11 +36,11 @@ public class TestController {
 
     @GetMapping("/myMethod2")
     public String myMethod2() {
-        System.out.println("Running myMethod()");
+        System.out.println("Running myMethod2()");
         return "Hello World2 - response value";
     }
 
-    // Convert JSON string to MyNum object using Jackson
+
     public static MyNum convertJsonToMyNum(String json) {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
